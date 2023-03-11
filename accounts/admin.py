ec7be81cf33d39497036127b67a896769dfabcdd
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-03-11 17:47:01 -0500

    In progress

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,5 +12,7 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = (
         'username',
-        'email'
+        'email',
+        'is_staff',
+        'is_superuser',
     )
