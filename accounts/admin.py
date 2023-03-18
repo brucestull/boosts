ec0f332ea266b35e788c584212e719e38538bda1
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-03-18 07:43:04 -0400

    Add 'registration_accepted' field to 'CustomUser', add field 'registration_accepted' to user change form in admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,8 +11,27 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = (
-        'username',
-        'email',
-        'is_staff',
-        'is_superuser',
+        "username",
+        # "email",
+        "registration_accepted",
+        "is_staff",
+        "is_superuser",
     )
+
+    def get_fieldsets(self, request, obj=None):
+        """
+        Override `get_fieldsets()` to add `registration_accepted` to a `Moderator Permissions` section of `CustomUser` change view.
+        """
+        # Get the default `fieldsets` from the superclass `UserAdmin`:
+        fieldsets = super().get_fieldsets(request, obj)
+
+        # Convert fieldsets to list:
+        fieldsets_as_list = list(fieldsets)
+
+        # Create list of single tuple for `registration_accepted`:
+        moderator_permissions_as_list = [
+            ("Moderator Permissions", {"fields": ("registration_accepted",)})
+        ]
+
+        # Combine the two lists and return the result:
+        return moderator_permissions_as_list + fieldsets_as_list
