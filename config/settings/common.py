001e3a61baf514cc81e9fa340224d5f1c4cba696
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-04-01 05:13:42 -0400

    Change 'LOGOUT_REDIRECT_URL' to 'login'.

--- config/settings/common.py
+++ config/settings/common.py
@@ -120,7 +120,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
