e5c25134de974f7f91b718d4dc7e828de3d42e34
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-01 06:09:43 -0400

    Add 'IsRegistrationAccepted' permission to 'inspirationals' view.

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from api.serializers import CurrentUserSerializer, InspirationalSerializer
+from api.permissions import IsRegistrationAccepted
 from boosts.models import Inspirational
 
 
@@ -18,5 +19,6 @@ class InspirationalsViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
     """
     View set for the Inspirational model.
     """
+    permission_classes = [IsRegistrationAccepted]
     serializer_class = InspirationalSerializer
     queryset = Inspirational.objects.all()
