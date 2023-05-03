c7404f1bb0fac634281aa8d806ce485a5de3c30e
Flynnt Knapp <bruce.stull@gmail.com>
2023-05-03 05:58:47 -0400

    Add temporary 'RedirectView' for server root to improve UX. Will modify this later to add custom 403 for users not 'registration_accepted'.

--- config/urls.py
+++ config/urls.py
@@ -16,19 +16,21 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 from config.settings.common import THE_SITE_NAME
 
 urlpatterns = [
-    path(
-        '',
-        TemplateView.as_view(
-            template_name="home.html",
-            extra_context={'the_site_name': THE_SITE_NAME},
-        ),
-        name='home',
-    ),
+    # path(
+    #     '',
+    #     TemplateView.as_view(
+    #         template_name="home.html",
+    #         extra_context={'the_site_name': THE_SITE_NAME},
+    #     ),
+    #     name='home',
+    # ),
+
+    path('', RedirectView.as_view(pattern_name='boosts:inspirational-list'), name='home'),
 
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
