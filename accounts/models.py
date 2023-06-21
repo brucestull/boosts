e36bea8ea55f2b61c85f4255f929b947db8ee110
Bruce Stull <bruce.stull@gmail.com>
2023-06-21 04:14:08 -0400

    Removed extraneous comment from model 'CustomUser'. Changed dependency 'psycopg2' to 'psycopg2-binary'.

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,6 @@ class CustomUser(AbstractUser):
     """
     A CustomUser class is added so we can add functionality later. It's more convenient then not to add CustomUser at beginning of project before database migrations are started.
     """
-    # Add additional fields in here and remove `pass` line below.
     registration_accepted = models.BooleanField(
         default=False,
     )
