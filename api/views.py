bae82cf38a8e8398260027f3afd75c8f4f1928f1
Bruce Stull <bruce.stull@gmail.com>
2023-07-26 18:07:17 -0400

    Have REST endpoint working with Django REST auth token

--- api/views.py
+++ api/views.py
@@ -2,29 +2,34 @@ from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from django.contrib.auth.mixins import LoginRequiredMixin
-
 from api.serializers import CurrentUserSerializer, InspirationalSerializer
 from api.permissions import IsRegistrationAccepted, IsStaff
+
 from boosts.models import Inspirational
 
 
-class CurrentUserViewSet(LoginRequiredMixin, generics.RetrieveAPIView):
+class CurrentUserViewSet(generics.RetrieveAPIView):
     serializer_class = CurrentUserSerializer
+
+    permission_classes = [
+        IsAuthenticated,
+        IsRegistrationAccepted,
+        IsStaff,
+    ]
 
     def get_object(self):
         return self.request.user
 
 
-# class InspirationalsViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
 class InspirationalsViewSet(viewsets.ModelViewSet):
     """
     View set for the Inspirational model.
     """
+
     permission_classes = [
         IsAuthenticated,
-    #     IsRegistrationAccepted,
-    #     IsStaff,
+        IsRegistrationAccepted,
+        IsStaff,
     ]
     serializer_class = InspirationalSerializer
     queryset = Inspirational.objects.all()
