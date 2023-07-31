c4a4dc28a88e84c9cc4d738ecea2a75d2105a5b1
Bruce Stull <bruce.stull@gmail.com>
2023-07-31 03:48:07 -0400

    Modified docstring

--- boosts/views.py
+++ boosts/views.py
@@ -26,9 +26,22 @@ class InspirationalListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     This view is only accessible to users who have `registration_accepted=True`.
     This is controlled by the `UserPassesTestMixin` and the `test_func` method.
 
-        The `LoginRequiredMixin` is used to route users to the login page if they are not logged in.
-        
-        The `UserPassesTestMixin` is used to ensure the user has `registration_accepted=True`.
+    Mixins:
+        LoginRequiredMixin: Ensures that the user is logged in. If not, they
+        are redirected to the login page.
+        UserPassesTestMixin: Ensures that the user has `registration_accepted=True`.
+        If not, they are prompted to login.
+
+    Attributes:
+        paginate_by (int): The number of objects to display per page.
+
+    Methods:
+        test_func: Test if user has `registration_accepted=True`. Only users
+        who pass this test can access this view.
+        get_queryset: Get the queryset for the view. Only the `Inspirational`
+        objects for the current user are returned.
+        get_context_data: Override the `get_context_data` method to add the
+        page title and the site name to the context.
     """
 
     paginate_by = 10
@@ -103,13 +116,11 @@ def send_inspirational(request, pk):
     inspirational = get_object_or_404(Inspirational, pk=pk)
     # Get the current site domain. This will resolve to a localhost in DEV and to the production domain in PROD:
     current_site = get_current_site(request)
-    plain_text_body = (
-        f"""
+    plain_text_body = f"""
             {inspirational.created.strftime("%y-%m-%d")} - {inspirational.body}
             \n
             Sent from https://{current_site.domain} by {request.user.username} ({request.user.email}).
         """
-    )
     # Send the inspirational quote to the user's beastie:
     send_mail(
         f"Inspirational Quote from your Beastie: {request.user.username}",
@@ -119,6 +130,8 @@ def send_inspirational(request, pk):
         # html_message=plain_text_body,
         fail_silently=False,
     )
-    messages.success(request, f"Sent '{inspirational.body[:20]}...' to your Beastie: {request.user.beastie.username}!")
+    messages.success(
+        request,
+        f"Sent '{inspirational.body[:20]}...' to your Beastie: {request.user.beastie.username}!",
+    )
     return redirect("boosts:inspirational-list")
-
