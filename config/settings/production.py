19028cf6c1b6ea4dde72e502068440b0ef66fe6a
FlynntKnapp <bruce.stull@gmail.com>
2023-11-01 06:07:25 -0400

    Move 'MIDDLEWARE' settings to 'common.py.

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,15 +2,8 @@ import os
 
 from config.settings.common import *
 
-MIDDLEWARE = MIDDLEWARE + ["whitenoise.middleware.WhiteNoiseMiddleware"]
-
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# Create a specific `SECRET_KEY` for production and use it in production only.
