5137befebf71272f2c70980c3dd415b84703bab4
FlynntKnapp <bruce.stull@gmail.com>
2023-11-03 05:58:53 -0400

    Move email settings to 'common.py.

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,10 +1,3 @@
 import os
 
 from config.settings.common import *  # noqa: F405, F403, F401
-
-# Email sending settings:
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
