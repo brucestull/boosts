7fb607f5646a07bf43c8b33d86319500a75d4325
FlynntKnapp <bruce.stull@gmail.com>
2023-11-05 07:57:06 -0500

    Add redis support for prod. Add 'django-redis' to dependencies.

--- config/settings.py
+++ config/settings.py
@@ -194,5 +194,5 @@ else:
 """
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
