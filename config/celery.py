b6a5f859833030bf3d4d924552f72a507b8522a2
FlynntKnapp <bruce.stull@gmail.com>
2023-11-05 07:16:31 -0500

    Add console logs for troubleshooting and learning.

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,19 @@
+from dotenv import load_dotenv
 import os
 
 import django
 from celery import Celery
+
+#######################################
+from django.conf import settings as s
+
+# print("s.EMAIL_HOST: ", s.EMAIL_HOST)
+# print("s.ENVIRONMENT: ", s.ENVIRONMENT)
+print("os.getenv('EMAIL_HOST'): ", os.getenv("EMAIL_HOST"))
+print("os.getenv('ENVIRONMENT'): ", os.getenv("ENVIRONMENT"))
+#######################################
+
+load_dotenv()
 
 # Define the default Django settings module for the 'celery' app.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
